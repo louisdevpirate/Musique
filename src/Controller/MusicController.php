dedcdb6cc763c565d1e3b7b32182f855cb4e3222
Louis Dole <louis.dole@live.fr>
2023-02-07 17:20:13 +0100

    Utilisation doctrine pour BDD Musique et affichage des données dans vue Twig

--- src/Controller/MusicController.php
+++ src/Controller/MusicController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Musique;
 
 class MusicController extends AbstractController
 {
@@ -18,11 +20,16 @@ class MusicController extends AbstractController
     }
 
     #[Route('/', name: 'app_home')]
-    public function home(): Response
+    public function home(ManagerRegistry $doctrine): Response
     {
+        $repo = $doctrine->getRepository(Musique::class);
+
+        $musiques = $repo->findAll();
+        
         return $this->render('music/home.html.twig', [
             'title' => 'Bienvenue à toi !',
             'age' => 31,
+            'musiques' => $musiques,
         ]);
     }
 }
